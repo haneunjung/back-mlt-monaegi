b4c0b1de6f19c1b67d7f24874bdb2aa51bee3143
Jeong-Uk Park <dnr1105@gmail.com>
2021-03-14 23:35:15 +0900

    Init

---
+++ src/main/kotlin/com/haneunjung/back/monaegi/MonaegiApplication.kt
@@ -0,0 +1,11 @@
+package com.haneunjung.back.monaegi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonaegiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MonaegiApplication>(*args)
+}
---
+++ src/test/kotlin/com/haneunjung/back/monaegi/MonaegiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.haneunjung.back.monaegi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonaegiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
